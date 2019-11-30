cafb9225684b58f097b8674df2a727b0b836f7ae
Dududenis <48683363+Dududenis@users.noreply.github.com>
2019-11-30 14:34:23 -0300

    Adicionando questões 7-8
    Adicionando questões 7-8

---
+++ questao_8.js
@@ -0,0 +1,40 @@
+function estado(nome){
+    for(let k=0; k<=26;k++){
+        if(nome==estados[k]){
+            return 'Estado encontrado'
+        }
+    }
+    return 'Estado não encontrado'
+}
+let estados = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Distrito Federal',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins',
+    ]
+
+    var nome = prompt('informe o estado corretamente:');
+    document.write(estado(nome))
