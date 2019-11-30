6b07c0a411e4decd67cebff17ecf6720623b73ad
Dududenis <48683363+Dududenis@users.noreply.github.com>
2019-11-30 14:41:13 -0300

    ADICIONANDO QUESTÕES 7-8

--- questao_8.js
+++ questao_8.js
@@ -1,6 +1,6 @@
 /*8. Dado o array da questão anterior, escreva um script que leia um nome de um dos estados ou do distrito federal e informe se o estado que ele procura está presente no array, o script 
-deve fazer o tratamento da string passada, exemplo: se o usuário digitar PaRaíBa o programa deve retornar que o mesmo existe, agora de for passado um nome que não se encontre no meu array 
-o programa deve retornar que o mesmo não se encontra.*/
+deve fazer o tratamento da string passada, exemplo: se o usuário digitar PaRaíBa o programa deve retornar que o mesmo existe, agora 
+de for passado um nome que não se encontre no meu array o programa deve retornar que o mesmo não se encontra.*/
 function estado(nome){
     for(let k=0; k<=26;k++){
         if(nome==estados[k]){
